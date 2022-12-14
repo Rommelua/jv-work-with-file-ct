4835632463362424052b16342cbf6373926e8ecb
OleksiiHubanov <oleksiyhubanov@gmail.com>
2022-12-14 23:20:15 +0200

    Task 2. WorkWithFile. Edited code. Commit 14.12.2022 23:20

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -15,34 +16,68 @@ public class WorkWithFile {
     }
 
     private void readFile(String fromFileName) {
-        try {
-            File csvFileReader = new File(fromFileName);
-            Scanner csvScanner = new Scanner(csvFileReader);
-            while (csvScanner.hasNextLine()) {
-                String firstLineFromFile = csvScanner.nextLine();
-                String[] separatedData = firstLineFromFile.split(",");
-                String operation = separatedData[0];
-                int number = Integer.parseInt(separatedData[1]);
-                if (operation.equals("supply")) {
-                    sumSupply = sumSupply + number;
-                } else {
-                    sumBuy = sumBuy + number;
-                }
-            }
-        } catch (Exception e) {
+        File csvFileReader = new File(fromFileName);
+        try (Scanner csvScanner = new Scanner(csvFileReader)) {
+            calculateData(csvScanner);
+        } catch (FileNotFoundException e) {
             throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+        }
+
+//        try {
+//            File csvFileReader = new File(fromFileName);
+//            Scanner csvScanner = new Scanner(csvFileReader);
+//            while (csvScanner.hasNextLine()) {
+//                String firstLineFromFile = csvScanner.nextLine();
+//                String[] separatedData = firstLineFromFile.split(",");
+//                String operation = separatedData[0];
+//                int number = Integer.parseInt(separatedData[1]);
+//                if (operation.equals("supply")) {
+//                    sumSupply = sumSupply + number;
+//                } else {
+//                    sumBuy = sumBuy + number;
+//                }
+//            }
+//            csvScanner.close();
+//        } catch (Exception e) {
+//            throw new RuntimeException("Can't read data from the file " + fromFileName, e);
+//        }
+    }
+
+    private void calculateData(Scanner csvScanner) {
+        while (csvScanner.hasNextLine()) {
+            String firstLineFromFile = csvScanner.nextLine();
+            String[] separatedData = firstLineFromFile.split(",");
+            String operation = separatedData[0];
+            int number = Integer.parseInt(separatedData[1]);
+            if (operation.equals("supply")) {
+                sumSupply = sumSupply + number;
+            } else {
+                sumBuy = sumBuy + number;
+            }
         }
     }
 
     private void createReport(String toFileName) {
-        try {
-            FileWriter csvFileWriter = new FileWriter(toFileName);
-            csvFileWriter.write("supply," + sumSupply + System.lineSeparator());
-            csvFileWriter.write("buy," + sumBuy + System.lineSeparator());
-            csvFileWriter.write("result," + (sumSupply - sumBuy));
-            csvFileWriter.close();
+        try (FileWriter csvFileWriter = new FileWriter(toFileName)){
+            writeToReport(csvFileWriter);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to the file " + toFileName, e);
         }
+
+//        try {
+//            FileWriter csvFileWriter = new FileWriter(toFileName);
+//            csvFileWriter.write("supply," + sumSupply + System.lineSeparator());
+//            csvFileWriter.write("buy," + sumBuy + System.lineSeparator());
+//            csvFileWriter.write("result," + (sumSupply - sumBuy));
+//            csvFileWriter.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException("Can't write data to the file " + toFileName, e);
+//        }
+    }
+
+    private void writeToReport(FileWriter csvFileWriter) throws IOException {
+        csvFileWriter.write("supply," + sumSupply + System.lineSeparator());
+        csvFileWriter.write("buy," + sumBuy + System.lineSeparator());
+        csvFileWriter.write("result," + (sumSupply - sumBuy));
     }
 }
